055172de4e5e1de27e46bf8a22f9fd2e0dd1db07
omersanik <omersanik2016@gmail.com>
2025-05-23 20:11:00 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Welcome</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Page;
